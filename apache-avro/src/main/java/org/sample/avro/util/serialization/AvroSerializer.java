d16d7399d17d0502c297ca4b1d9b7f1f93111c00
Avinash Yadav <aviyadav@users.noreply.github.com>
2018-12-03 16:58:49 +0530

    AVRO Sample
    AVRO Sample

---
+++ apache-avro/src/main/java/org/sample/avro/util/serialization/AvroSerializer.java
@@ -0,0 +1,51 @@
+package org.sample.avro.util.serialization;
+
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.sample.avro.util.model.AvroHttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class AvroSerializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(AvroSerializer.class);
+
+    public byte[] serealizeAvroHttpRequestJSON(AvroHttpRequest request) {
+        DatumWriter<AvroHttpRequest> writer = new SpecificDatumWriter<>(AvroHttpRequest.class);
+        byte[] data = new byte[0];
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Encoder jsonEncoder = null;
+        try {
+            jsonEncoder = EncoderFactory.get()
+                    .jsonEncoder(AvroHttpRequest.getClassSchema(), stream);
+            writer.write(request, jsonEncoder);
+            jsonEncoder.flush();
+            data = stream.toByteArray();
+        } catch (IOException e) {
+            logger.error("Serialization error " + e.getMessage());
+        }
+        return data;
+    }
+
+    public byte[] serealizeAvroHttpRequestBinary(AvroHttpRequest request) {
+        DatumWriter<AvroHttpRequest> writer = new SpecificDatumWriter<>(AvroHttpRequest.class);
+        byte[] data = new byte[0];
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Encoder jsonEncoder = EncoderFactory.get()
+                .binaryEncoder(stream, null);
+        try {
+            writer.write(request, jsonEncoder);
+            jsonEncoder.flush();
+            data = stream.toByteArray();
+        } catch (IOException e) {
+            logger.error("Serialization error " + e.getMessage());
+        }
+
+        return data;
+    }
+}
